108ccee227800e0e300cafe135c6dda7f96d1f35
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-22 20:04:59 +0200

    Changed namespace declaration to the file-scoped one

--- Claims/Auditing/AuditContext.cs
+++ Claims/Auditing/AuditContext.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace Claims.Auditing
-{
-    public class AuditContext : DbContext
-    {
-        public AuditContext(DbContextOptions<AuditContext> options)
-            : base(options)
-        {
-        }
+namespace Claims.Auditing;
 
-        public DbSet<ClaimAudit> ClaimAudits { get; set; }
-        public DbSet<CoverAudit> CoverAudits { get; set; }
+public class AuditContext : DbContext
+{
+    public AuditContext(DbContextOptions<AuditContext> options)
+        : base(options)
+    {
     }
+
+    public DbSet<ClaimAudit> ClaimAudits { get; set; }
+    public DbSet<CoverAudit> CoverAudits { get; set; }
 }
