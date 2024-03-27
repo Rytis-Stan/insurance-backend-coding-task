d94c105e63793c167bef3eb605de5058e6f68be3
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-27 15:37:30 +0200

    Introducing DB-specific versions of "Claim" and "Cover" types (adding DB entities)

---
+++ Claims/DataAccess/ClaimJson.cs
@@ -0,0 +1,25 @@
+﻿using Claims.Domain;
+using Newtonsoft.Json;
+
+namespace Claims.DataAccess;
+
+public class ClaimJson
+{
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+
+    [JsonProperty(PropertyName = "coverId")]
+    public string CoverId { get; set; }
+
+    [JsonProperty(PropertyName = "created")]
+    public DateTime Created { get; set; }
+
+    [JsonProperty(PropertyName = "name")]
+    public string Name { get; set; }
+
+    [JsonProperty(PropertyName = "claimType")]
+    public ClaimType Type { get; set; }
+
+    [JsonProperty(PropertyName = "damageCost")]
+    public decimal DamageCost { get; set; }
+}
---
+++ Claims/DataAccess/CoverJson.cs
@@ -0,0 +1,23 @@
+﻿using Claims.Domain;
+using Newtonsoft.Json;
+
+namespace Claims.DataAccess;
+
+public class CoverJson
+{
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+
+    [JsonProperty(PropertyName = "startDate")]
+    public DateOnly StartDate { get; set; }
+
+    [JsonProperty(PropertyName = "endDate")]
+    public DateOnly EndDate { get; set; }
+
+    [JsonProperty(PropertyName = "claimType")]
+    public CoverType Type { get; set; }
+
+    // TODO: Use decimal???
+    [JsonProperty(PropertyName = "premium")]
+    public decimal Premium { get; set; }
+}
