7f71c66fd29bcd811dc9401dc8ee7b955f2dbd4e
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-27 15:26:51 +0200

    Refactoring code

--- Claims.Tests/ClaimsControllerTests.cs
+++ Claims.Tests/ClaimsControllerTests.cs
@@ -8,15 +8,20 @@ public class ClaimsControllerTests
     [Fact]
     public async Task Get_Claims()
     {
-        var application = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(_ => {});
-
-        var client = application.CreateClient();
+        var client = CreateClient();
 
         var response = await client.GetAsync("/Claims");
 
         response.EnsureSuccessStatusCode();
 
         //TODO: Apart from ensuring 200 OK being returned, what else can be asserted?
+    }
+
+    private HttpClient CreateClient()
+    {
+        var application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(_ => {});
+
+        return application.CreateClient();
     }
 }
