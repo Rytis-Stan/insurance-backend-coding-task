7c7d835fff96efdf6e67fb502af0345eebacf4fc
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-28 17:10:33 +0200

    Removed unnecessary "JsonProperty" attributes

--- Claims/DataAccess/ClaimJson.cs
+++ Claims/DataAccess/ClaimJson.cs
@@ -8,18 +8,9 @@ public class ClaimJson
     [JsonProperty(PropertyName = "id")]
     public required string Id { get; init; }
 
-    [JsonProperty(PropertyName = "coverId")]
     public required string CoverId { get; init; }
-
-    [JsonProperty(PropertyName = "name")]
     public required string Name { get; init; }
-
-    [JsonProperty(PropertyName = "claimType")]
     public required ClaimType Type { get; init; }
-
-    [JsonProperty(PropertyName = "damageCost")]
     public required decimal DamageCost { get; init; }
-
-    [JsonProperty(PropertyName = "created")]
     public required DateTime Created { get; init; }
 }
--- Claims/DataAccess/CoverJson.cs
+++ Claims/DataAccess/CoverJson.cs
@@ -8,19 +8,9 @@ public class CoverJson
     [JsonProperty(PropertyName = "id")]
     public required string Id { get; init; }
 
-    [JsonProperty(PropertyName = "startDate")]
     public required DateOnly StartDate { get; init; }
-
-    [JsonProperty(PropertyName = "endDate")]
     public required DateOnly EndDate { get; init; }
-
-    [JsonProperty(PropertyName = "claimType")]
     public required CoverType Type { get; init; }
-
-    // TODO: Use decimal???
-    [JsonProperty(PropertyName = "premium")]
     public required decimal Premium { get; init; }
-
-    [JsonProperty(PropertyName = "created")]
     public required DateTime Created { get; init; }
 }
