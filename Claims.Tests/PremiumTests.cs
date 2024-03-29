2e30565bfb1d4427ccd4b46e884190b2c4e7f8f2
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-29 11:21:55 +0200

    Changing premium calculation tests to use randomly generated dates, to simplify tests

--- Claims.Tests/PremiumTests.cs
+++ Claims.Tests/PremiumTests.cs
@@ -6,18 +6,25 @@ namespace Claims.Tests;
 // TODO: Rename the test class when the premium calculation logic gets placed in its proper place.
 public class PremiumTests
 {
+    private static readonly Random Random = new Random();
+
     [Theory]
-    [InlineData(2000, 01, 01, CoverType.Yacht, 1375)]         // 10% of 1250
-    [InlineData(2000, 01, 01, CoverType.PassengerShip, 1500)] // 20% of 1250
-    [InlineData(2000, 01, 01, CoverType.Tanker, 1875)]        // 50% of 1250
-    [InlineData(2000, 01, 01, CoverType.ContainerShip, 1625)] // 30% of 1250
-    [InlineData(2000, 01, 01, CoverType.BulkCarrier, 1625)]   // 30% of 1250
-    public void PremiumForASingleDayIsEqualToBaseDayRateForTheSpecificCoverType(int year, int month, int day, CoverType coverType, decimal expectedPremium)
+    [InlineData(CoverType.Yacht, 1375)]         // 10% of 1250
+    [InlineData(CoverType.PassengerShip, 1500)] // 20% of 1250
+    [InlineData(CoverType.Tanker, 1875)]        // 50% of 1250
+    [InlineData(CoverType.ContainerShip, 1625)] // 30% of 1250
+    [InlineData(CoverType.BulkCarrier, 1625)]   // 30% of 1250
+    public void PremiumForASingleDayIsEqualToBaseDayRateForTheSpecificCoverType(CoverType coverType, decimal expectedPremium)
     {
-        var startAndEndDate = new DateOnly(year, month, day);
+        var startAndEndDate = RandomDate();
         Assert.Equal(
             expectedPremium,
             Cover.ComputePremium(startAndEndDate, startAndEndDate, coverType)
         );
+    }
+
+    private DateOnly RandomDate()
+    {
+        return new DateOnly(2000, 01, 01).AddDays(Random.Next(10_000));
     }
 }
