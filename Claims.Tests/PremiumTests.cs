1e66f973a8c010f14f6fabcf26903c40bd14275b
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-29 11:11:04 +0200

    Implementings tests for premium calculations

--- Claims.Tests/PremiumTests.cs
+++ Claims.Tests/PremiumTests.cs
@@ -6,21 +6,18 @@ namespace Claims.Tests;
 // TODO: Rename the test class when the premium calculation logic gets placed in its proper place.
 public class PremiumTests
 {
-    [Fact]
-    public void PremiumForASingleDayIsEqualToBaseDayRateForTheSpecificCoverType()
+    [Theory]
+    [InlineData(2000, 01, 01, CoverType.Yacht, 1375)]         // 10% of 1250
+    [InlineData(2000, 01, 01, CoverType.PassengerShip, 1500)] // 20% of 1250
+    [InlineData(2000, 01, 01, CoverType.Tanker, 1875)]        // 50% of 1250
+    [InlineData(2000, 01, 01, CoverType.ContainerShip, 1625)] // 30% of 1250
+    [InlineData(2000, 01, 01, CoverType.BulkCarrier, 1625)]   // 30% of 1250
+    public void PremiumForASingleDayIsEqualToBaseDayRateForTheSpecificCoverType(int year, int month, int day, CoverType coverType, decimal expectedPremium)
     {
-        Test(new DateOnly(2000, 01, 01), CoverType.Yacht, 1375);         // 10% of 1250
-        Test(new DateOnly(2000, 01, 01), CoverType.PassengerShip, 1500); // 20% of 1250
-        Test(new DateOnly(2000, 01, 01), CoverType.Tanker, 1875);        // 50% of 1250
-        Test(new DateOnly(2000, 01, 01), CoverType.ContainerShip, 1625); // 30% of 1250
-        Test(new DateOnly(2000, 01, 01), CoverType.BulkCarrier, 1625);   // 30% of 1250
-
-        void Test(DateOnly startAndEndDate, CoverType coverType, decimal expectedPremium)
-        {
-            Assert.Equal(
-                expectedPremium,
-                Cover.ComputePremium(startAndEndDate, startAndEndDate, coverType)
-            );
-        }
+        var startAndEndDate = new DateOnly(year, month, day);
+        Assert.Equal(
+            expectedPremium,
+            Cover.ComputePremium(startAndEndDate, startAndEndDate, coverType)
+        );
     }
 }
