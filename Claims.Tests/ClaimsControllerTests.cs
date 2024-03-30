30d3bbb535cc7e5702f9c219b8ac9508d4afda88
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-30 13:48:24 +0200

    Implemeting API tests

--- Claims.Tests/ClaimsControllerTests.cs
+++ Claims.Tests/ClaimsControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using Claims.Domain;
+using Claims.Dto;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -6,6 +8,24 @@ namespace Claims.Tests;
 
 public class ClaimsControllerTests : ControllerTests
 {
+    [Fact]
+    public async Task ClaimsPostReturnsNewlyCreatedClaim()
+    {
+        var name = "someRandomName";
+        var claimType = ClaimType.BadWeather;
+        var damageCost = 123;
+        var dateTime = new DateTime(2000, 01, 01);
+
+        var response = await Client.PostAsync("/Claims", new CreateClaimRequestDto(Guid.NewGuid(), name, claimType, damageCost, dateTime));
+
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var claim = JsonConvert.DeserializeObject<object>(content);
+
+        Assert.NotNull(claim);
+    }
+
     [Fact]
     public async Task ClaimsGetReturnEmptyClaimCollectionWhenNoClaimsAdded()
     {
--- Claims.Tests/ControllerTests.cs
+++ Claims.Tests/ControllerTests.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Claims.Domain;
+using Claims.Dto;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace Claims.Tests;
 
@@ -40,5 +46,16 @@ public abstract class ControllerTests : IDisposable
 
             return base.CreateHost(builder);
         }
+    }
+}
+
+public static class HttpClientExtensions
+{
+    public static async Task<HttpResponseMessage> PostAsync<T>(this HttpClient client, [StringSyntax(StringSyntaxAttribute.Uri)] string requestUri, T content)
+    {
+        string json = JsonConvert.SerializeObject(content);
+        var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync("/Claims", httpContent);
+        return response;
     }
 }
