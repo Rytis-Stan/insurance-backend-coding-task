20bb6d49d3665255b20b1178918682fbc421b9b8
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-30 18:58:38 +0200

    Adding test coverage for "CoversService"

--- Claims.Tests/ClaimsServiceTests.cs
+++ Claims.Tests/ClaimsServiceTests.cs
@@ -7,6 +7,9 @@ namespace Claims.Tests;
 public class ClaimsServiceTests
 {
     private const ClaimType AnyClaimType = ClaimType.BadWeather;
+    private const decimal AnyDamageCost = 10.00m;
+    private const CoverType AnyCoverType = CoverType.BulkCarrier;
+    private const decimal AnyPremium = 100.00m;
 
     [Theory]
     [InlineData(100_001)]
@@ -31,13 +34,49 @@ public class ClaimsServiceTests
     {
         var coverId = Guid.NewGuid();
         var created = new DateTime(2000, 01, 01); // TODO: Random date-time value within a valid range?
-        var request = new CreateClaimRequestDto(coverId, "anyName", AnyClaimType, 100, created);
+        var request = new CreateClaimRequestDto(coverId, "anyName", AnyClaimType, AnyDamageCost, created);
         var service = new ClaimsService(new ClaimsRepositoryStub(), new CoversRepositoryStub(coverId, null));
 
         await AssertExtended.ThrowsArgumentExceptionAsync(
             () => service.CreateClaimAsync(request),
             "Claim references a non-existing cover via the cover ID."
         );
+    }
+
+    [Fact]
+    public async Task ThrowsExceptionWhenCreatingAClaimForADateOutsideOfRelatedCoverPeriod()
+    {
+        // TODO: Use UTC datetime!!!
+        await Test(
+            new DateOnly(2000, 10, 20),
+            new DateOnly(2000, 10, 20),
+            new DateTime(2000, 10, 18)
+        );
+        await Test(
+            new DateOnly(2000, 10, 20),
+            new DateOnly(2000, 10, 20),
+            new DateTime(2000, 10, 19)
+        );
+
+        async Task Test(DateOnly coverStartDate, DateOnly coverEndDate, DateTime claimCreated)
+        {
+            var coverId = Guid.NewGuid();
+            var request = new CreateClaimRequestDto(coverId, "anyName", AnyClaimType, 100, claimCreated);
+            var cover = new Cover
+            {
+                Id = coverId,
+                StartDate = coverStartDate,
+                EndDate = coverEndDate,
+                Type = AnyCoverType,
+                Premium = AnyPremium
+            };
+            var service = new ClaimsService(new ClaimsRepositoryStub(), new CoversRepositoryStub(coverId, cover));
+
+            await AssertExtended.ThrowsArgumentExceptionAsync(
+                () => service.CreateClaimAsync(request),
+                "Claim is outside of the related cover period."
+            );
+        }
     }
 
     private class ClaimsRepositoryStub : IClaimsRepository
