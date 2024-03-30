d9c41f6c736848bbf7db698465cf59a8e668a530
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-30 14:00:12 +0200

    Moved out "HttpClientExtensions" class into it's own file

--- Claims.Tests/ControllerTests.cs
+++ Claims.Tests/ControllerTests.cs
@@ -1,9 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Text;
-using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
 
 namespace Claims.Tests;
 
@@ -43,27 +40,5 @@ public abstract class ControllerTests : IDisposable
 
             return base.CreateHost(builder);
         }
-    }
-}
-
-public static class HttpClientExtensions
-{
-    public static async Task<HttpResponseMessage> PostAsync<T>(this HttpClient client, [StringSyntax(StringSyntaxAttribute.Uri)] string requestUri, T content)
-    {
-        string json = JsonConvert.SerializeObject(content);
-        var response = await client.PostAsync(requestUri, JsonContent(json));
-        return response;
-    }
-
-    private static StringContent JsonContent(string json)
-    {
-        return new StringContent(json, Encoding.UTF8, "application/json");
-    }
-
-    public static async Task<T?> ReadContentAsync<T>(this HttpResponseMessage response)
-        where T : class
-    {
-        var content = await response.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<T>(content);
     }
 }
---
+++ Claims.Tests/HttpClientExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Claims.Tests;
+
+public static class HttpClientExtensions
+{
+    public static async Task<HttpResponseMessage> PostAsync<T>(this HttpClient client, [StringSyntax(StringSyntaxAttribute.Uri)] string requestUri, T content)
+    {
+        string json = JsonConvert.SerializeObject(content);
+        var response = await client.PostAsync(requestUri, JsonContent(json));
+        return response;
+    }
+
+    private static StringContent JsonContent(string json)
+    {
+        return new StringContent(json, Encoding.UTF8, "application/json");
+    }
+
+    public static async Task<T?> ReadContentAsync<T>(this HttpResponseMessage response)
+        where T : class
+    {
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<T>(content);
+    }
+}
