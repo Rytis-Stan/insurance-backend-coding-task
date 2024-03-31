b91c8f5fefc26fe090dbf88e18d098e3766a4f5e
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-03-31 12:58:54 +0300

    Added an initial version of Rabbit MQ message consumer

---
+++ Claims.AuditDaemon/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+namespace Claims.AuditDaemon;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        // TODO: Move the queue name (and some options???) to the configuration file!
+        const string queueName = "Claims.AuditQueue";
+        channel.QueueDeclare(
+            queue: queueName,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, eventArgs) =>
+        {
+            var body = eventArgs.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            var cccc = message;
+        };
+
+        channel.BasicConsume(
+            queue: queueName,
+            autoAck: true,
+            consumer: consumer
+        );
+    }
+}
