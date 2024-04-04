12bdcf8ca3c9a9b0acccf24ac9b1ce47b17bc70a
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-04-04 13:44:39 +0300

    Disabled "Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable." warnings for strongly-typed configuration classes

--- Source/Main/Claims.AuditDaemon/Configuration/AppConfiguration.cs
+++ Source/Main/Claims.AuditDaemon/Configuration/AppConfiguration.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Configuration;
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace Claims.AuditDaemon.Configuration;
 
--- Source/Main/Claims.AuditDaemon/Configuration/RabbitMqConfiguration.cs
+++ Source/Main/Claims.AuditDaemon/Configuration/RabbitMqConfiguration.cs
@@ -1,4 +1,5 @@
-﻿namespace Claims.AuditDaemon.Configuration;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+namespace Claims.AuditDaemon.Configuration;
 
 public class RabbitMqConfiguration
 {
