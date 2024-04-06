db3740c3008925fb58cef23f6664e4b902285e8e
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-04-06 22:02:24 +0300

    Changed the text that gets displayed when a message from the message queue comes in

--- Source/Main/BuildingBlocks.MessageQueues/LoggingQueueListener.cs
+++ Source/Main/BuildingBlocks.MessageQueues/LoggingQueueListener.cs
@@ -5,6 +5,6 @@ public class LoggingQueueListener<TMessage> : IQueueListener<TMessage>
     public void OnMessageReceived(TMessage message)
     {
         // TODO: Make the console writer an injectable dependency or change this into an actual log instance.
-        Console.WriteLine($"RECEIVED_AT: {DateTime.UtcNow}, MESSAGE: {message}");
+        Console.WriteLine($"[{DateTime.UtcNow:yyyy-MM-dd}]. Received message: {message}");
     }
 }
