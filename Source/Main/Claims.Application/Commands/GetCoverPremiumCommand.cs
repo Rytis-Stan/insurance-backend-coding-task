efd44664be74a029ba3582277b5f517d28e8d45d
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-04-06 16:07:51 +0300

    Implemented the "GetCoverPremiumCommand" class

---
+++ Source/Main/Claims.Application/Commands/GetCoverPremiumCommand.cs
@@ -0,0 +1,20 @@
+﻿using Claims.Domain;
+
+namespace Claims.Application.Commands;
+
+public record GetCoverPremiumRequest(DateOnly StartDate, DateOnly EndDate, CoverType CoverType);
+
+public class GetCoverPremiumCommand : ICommand<GetCoverPremiumRequest, decimal>
+{
+    private readonly ICoverPricing _coverPricing;
+
+    public GetCoverPremiumCommand(ICoverPricing coverPricing)
+    {
+        _coverPricing = coverPricing;
+    }
+
+    public async Task<decimal> ExecuteAsync(GetCoverPremiumRequest request)
+    {
+        return _coverPricing.CalculatePremium(request.StartDate, request.EndDate, request.CoverType);
+    }
+}
---
+++ Source/Tests/Claims.Application.Tests/Commands/GetCoverPremiumCommandTests.cs
@@ -0,0 +1,41 @@
+﻿using Claims.Application.Commands;
+using Claims.Domain;
+using Claims.Testing;
+using Moq;
+using Xunit;
+
+namespace Claims.Application.Tests.Commands;
+
+public class GetCoverPremiumCommandTests
+{
+    private readonly Mock<ICoverPricing> _coverPricingMock;
+    private readonly GetCoverPremiumCommand _command;
+
+    public GetCoverPremiumCommandTests()
+    {
+        _coverPricingMock = new Mock<ICoverPricing>();
+        _command = new GetCoverPremiumCommand(_coverPricingMock.Object);
+    }
+
+    [Theory]
+    [InlineData(12.34)]
+    [InlineData(456.78)]
+    public async Task ReturnsPremiumFromCoverPricing(decimal expectedPremium)
+    {
+        var startDate = TestData.RandomDate();
+        var endDate = TestData.RandomDate();
+        var coverType = TestData.RandomEnum<CoverType>();
+        StubPremium(startDate, endDate, coverType, expectedPremium);
+
+        var returnedPremium = await _command.ExecuteAsync(new GetCoverPremiumRequest(startDate, endDate, coverType));
+
+        Assert.Equal(expectedPremium, returnedPremium);
+    }
+
+    private void StubPremium(DateOnly startDate, DateOnly endDate, CoverType coverType, decimal premium)
+    {
+        _coverPricingMock
+            .Setup(x => x.CalculatePremium(startDate, endDate, coverType))
+            .Returns(premium);
+    }
+}
