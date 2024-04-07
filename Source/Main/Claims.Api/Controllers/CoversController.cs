c88e431fe08145110b35ef61d9f22d5ed456f958
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-04-07 16:09:41 +0300

    Moving command dependency injection code to controller actions instead of the constructor

--- Source/Main/Claims.Api/Controllers/CoversController.cs
+++ Source/Main/Claims.Api/Controllers/CoversController.cs
@@ -9,37 +9,26 @@ namespace Claims.Api.Controllers;
 [Route("[controller]")]
 public class CoversController : ControllerBase
 {
-    private readonly ICreateCoverCommand _createCoverCommand;
-    private readonly IGetCoverCommand _getCoverCommand;
-    private readonly IGetAllCoversCommand _getAllCoversCommand;
-    private readonly IDeleteCoverCommand _deleteCoverCommand;
-    private readonly IGetCoverPremiumCommand _getCoverPremiumCommand;
     private readonly ICoverAuditor _coverAuditor;
 
-    // TODO: Fix issue of too many controller constructor parameters (and do the same for the other controller)!
-    public CoversController(ICreateCoverCommand createCoverCommand, IGetCoverCommand getCoverCommand, IGetAllCoversCommand getAllCoversCommand, IDeleteCoverCommand deleteCoverCommand, IGetCoverPremiumCommand getCoverPremiumCommand, ICoverAuditor coverAuditor)
+    public CoversController(ICoverAuditor coverAuditor)
     {
-        _createCoverCommand = createCoverCommand;
-        _getCoverCommand = getCoverCommand;
-        _getAllCoversCommand = getAllCoversCommand;
-        _deleteCoverCommand = deleteCoverCommand;
-        _getCoverPremiumCommand = getCoverPremiumCommand;
         _coverAuditor = coverAuditor;
     }
 
     [HttpPost]
-    public async Task<ActionResult<CoverDto>> CreateAsync(CreateCoverRequestDto request)
+    public async Task<ActionResult<CoverDto>> CreateAsync([FromServices] ICreateCoverCommand command, CreateCoverRequestDto request)
     {
-        var response = await _createCoverCommand.ExecuteAsync(request.ToDomainRequest());
+        var response = await command.ExecuteAsync(request.ToDomainRequest());
         var cover = response.Cover;
         _coverAuditor.AuditPost(cover.Id);
         return Ok(cover.ToDto());
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<CoverDto>> GetAsync(Guid id)
+    public async Task<ActionResult<CoverDto>> GetAsync([FromServices] IGetCoverCommand command, Guid id)
     {
-        var response = await _getCoverCommand.ExecuteAsync(new GetCoverRequest(id));
+        var response = await command.ExecuteAsync(new GetCoverRequest(id));
         var cover = response.Cover;
         return cover != null
             ? Ok(cover.ToDto())
@@ -47,25 +36,26 @@ public class CoversController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<CoverDto>>> GetAsync()
+    public async Task<ActionResult<IEnumerable<CoverDto>>> GetAsync([FromServices] IGetAllCoversCommand command)
     {
-        var response = await _getAllCoversCommand.ExecuteAsync();
+        var response = await command.ExecuteAsync();
         var covers = response.Covers;
         return Ok(covers.Select(x => x.ToDto()));
     }
 
     [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteAsync(Guid id)
+    public async Task<ActionResult> DeleteAsync([FromServices] IDeleteCoverCommand command, Guid id)
     {
-        await _deleteCoverCommand.ExecuteAsync(new DeleteCoverRequest(id));
+        await command.ExecuteAsync(new DeleteCoverRequest(id));
         _coverAuditor.AuditDelete(id);
         return NoContent();
     }
 
     [HttpGet("Premium")]
-    public async Task<ActionResult<decimal>> GetCoverPremiumAsync(DateOnly startDate, DateOnly endDate, CoverTypeDto coverType)
+    public async Task<ActionResult<decimal>> GetCoverPremiumAsync([FromServices] IGetCoverPremiumCommand command,
+        DateOnly startDate, DateOnly endDate, CoverTypeDto coverType)
     {
-        var response = await _getCoverPremiumCommand.ExecuteAsync(new GetCoverPremiumRequest(startDate, endDate, coverType.ToDomainEnum()));
+        var response = await command.ExecuteAsync(new GetCoverPremiumRequest(startDate, endDate, coverType.ToDomainEnum()));
         return Ok(response.Premium);
     }
 }
