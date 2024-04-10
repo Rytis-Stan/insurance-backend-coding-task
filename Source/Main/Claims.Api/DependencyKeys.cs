ebc57541bc44877566224b9654a436b58225e73a
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-04-10 03:49:30 +0300

    Removed the temporary "DependenceyKeys" class

--- Source/Main/Claims.Api/DependencyKeys.cs
+++
@@ -1,7 +0,0 @@
-﻿// namespace Claims.Api;
-//
-// public class DependencyKeys
-// {
-//     public const string ClaimAuditor = nameof(ClaimAuditor);
-//     public const string CoverAuditor = nameof(CoverAuditor);
-// }
