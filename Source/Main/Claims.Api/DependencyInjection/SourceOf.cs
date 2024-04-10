3f4df81716d1abf8500049aef7ee992375891d9c
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-04-10 06:02:21 +0300

    Code comment fixes

--- Source/Main/Claims.Api/DependencyInjection/SourceOf.cs
+++ Source/Main/Claims.Api/DependencyInjection/SourceOf.cs
@@ -5,10 +5,10 @@ namespace Claims.Api.DependencyInjection;
 /// A helper type to make it possible to inject dependencies that implement the same interface
 /// but which require different implementations to be injected into different places.
 ///
-/// For each type of injection that should be injected, a class derived from this one should be
-/// created with the appropriate name and should be used instead of the original dependency both
-/// at the site of where it is registered and also where it gets injected. Getting to the real
-/// dependency can be done by simply using the "Obj" property.
+/// For each type that should be injected, a class derived from this one should be created with
+/// the appropriate name and should be used instead of the original dependency both at the site
+/// of where it is registered and also where it gets injected. Getting to the real dependency
+/// can be done by simply using the "Obj" property.
 /// </summary>
 /// <remarks>
 /// One solution for injecting different implementation for the same interface would be to just
