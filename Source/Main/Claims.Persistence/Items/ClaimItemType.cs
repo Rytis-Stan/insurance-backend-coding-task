f9e198ef18dcd52075dcde22b24b8df085b2a68a
Rytis-Stan <rytis.stankevicius.2@gmail.com>
2024-04-10 04:34:01 +0300

    Added JSON serialization attributes to Cosmos DB enums so that their values would be saved using labels instead of just numeric values

--- Source/Main/Claims.Persistence/Items/ClaimItemType.cs
+++ Source/Main/Claims.Persistence/Items/ClaimItemType.cs
@@ -1,5 +1,9 @@
-﻿namespace Claims.Persistence.Items;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
+namespace Claims.Persistence.Items;
+
+[JsonConverter(typeof(StringEnumConverter))]
 public enum ClaimItemType
 {
     Collision,
